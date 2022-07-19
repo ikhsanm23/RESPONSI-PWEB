1e6282647acb137156785a07c3b783583b22e9d7
Ikhsan Maulana <101860073+ikhsanm23@users.noreply.github.com>
2022-07-19 15:52:01 +0700

    Add files via upload

---
+++ input-output.php
@@ -0,0 +1,36 @@
+		 <div align="center"><h2>Isi Data Diri Anda</h2></div>
+		           <form name="nama" method="post" action="proses.php">
+			        <table width="58%" border="0" align="center">
+				    <tr>
+					    <td>Nama lengkap</td>
+					    <td><input type="text" name="nama" id="nama"></td>
+				    </tr>
+				    <tr>
+					    <td>NIM</td>
+					    <td><input type="text" name="nim" id="nim"></td>
+				    </tr>
+				    <tr>
+					    <td>E-mail</td>
+					    <td><input type="text" name="email" id="email"></td>
+				    </tr>
+				    <tr>
+					<td>Prodi</td>
+					<td><select name="prodi" id="prodi">
+						<option>Informatika</option>
+						<option>Hukum</option>
+						<option>Matematika</option>
+						<option>Manajemen</option>
+						<option>Sistem Informasi</option>
+					</select></td>
+				</tr>
+				<tr>
+					<td>&nbsp</td>
+					<td><input type="submit" name="submit" value="Kirim" id="kirim"><input type="reset" name="reset" value="Batal"></td>
+				</tr>
+				
+			</table>
+		</form>
+		<br><br>
+		<div align="center"><a href="input-output.php"><b>Lihat Data Diri<b></a></div>
+			</div>
+		</div>
